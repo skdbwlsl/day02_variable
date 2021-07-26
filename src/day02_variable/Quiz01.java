34706f1c0fc7d1605e903e26efabc2b5398b7b53
dbwls <youjin9874@naver.com>
2021-07-26 12:43:29 +0900

    기본 변수의 이해

---
+++ src/day02_variable/Quiz01.java
@@ -0,0 +1,16 @@
+package day02_variable;
+
+public class Quiz01 {
+	public static void main(String[] args) {
+		byte b = 97;
+		short s  = 20;
+		char c = 'A';
+		float f;
+		s = b;
+		c = b;
+		s = c;
+		c = s;
+		f = 5.11;
+	}
+
+}
